3950bb3a8bc0791aab70803ca38f047e66a25a3e
doa5 <odiri101@gmail.com>
2026-09-11 16:38:51 +0100

    feat: add taiga dogo

---
+++ spell-engine/TaigaDojo.cs
@@ -0,0 +1,12 @@
+namespace NasuverseSpellEngine
+{
+    public class TaigaDojo
+    {
+        public int TargetHP { get; set; }
+
+        public TaigaDojo(int initialHP = 100)
+        {
+            TargetHP = initialHP;
+        }
+    }
+}
