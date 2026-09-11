790a4650ac1080c4980a87d5d2f287bceb1ff740
doa5 <odiri101@gmail.com>
2026-09-11 16:38:41 +0100

    feat: add resource pool with Consume & Add mana methods

---
+++ spell-engine/ResourcePool.cs
@@ -0,0 +1,29 @@
+namespace NasuverseSpellEngine
+{
+    public class ResourcePool
+    {
+        public int Mana { get; private set; }
+
+        public ResourcePool(int initialMana = 50)
+        {
+            Mana = initialMana;
+        }
+
+        public bool TryConsume(int amount)
+        {
+            if (amount < 0) return false;
+            if (Mana >= amount)
+            {
+                Mana -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public void Add(int amount)
+        {
+            if (amount < 0) return;
+            Mana += amount;
+        }
+    }
+}
