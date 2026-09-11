841fd9f7fcc1152b9cf7cf9578f6dc5994fcaea5
doa5 <odiri101@gmail.com>
2026-09-11 17:34:49 +0100

    feat: convert to top-level statement (modern style apparently)

--- spell-engine/Program.cs
+++ spell-engine/Program.cs
@@ -1,27 +1,20 @@
-﻿namespace NasuverseSpellEngine
+﻿using NasuverseSpellEngine;
+
+TaigaDojo dojo = new TaigaDojo(100);
+Character aoko = CreateAoko();
+
+Console.WriteLine($"Welcome to the Taiga Dojo with {aoko.Name}!");
+Console.WriteLine($"Initial Mana: {aoko.Resources.Mana}, Dojo HP: {dojo.TargetHP}\n");
+
+static Character CreateAoko()
 {
-    class Program 
-    { 
-        static Character CreateAoko()
-        {
-            Character aoko = new Character("Aoko", new ResourcePool(initialMana: 100));
+    Character aoko = new Character("Aoko", new ResourcePool(initialMana: 100));
 
-            Spell snapAndDraw = new Spell("Snap & Draw", manaCost: 20, damage: 5);
-            Spell earthlightStarbow = new Spell("Earthlight Starbow", manaCost: 50, damage: 35);
+    Spell snapAndDraw = new Spell("Snap & Draw", manaCost: 20, damage: 5);
+    Spell earthlightStarbow = new Spell("Earthlight Starbow", manaCost: 50, damage: 35);
 
-            aoko.AvailableSpells.Add(earthlightStarbow);
-            aoko.AvailableSpells.Add(snapAndDraw);
+    aoko.AvailableSpells.Add(earthlightStarbow);
+    aoko.AvailableSpells.Add(snapAndDraw);
 
-            return aoko;
-        }
-        static void Main(string[] args)
-        {
-            TaigaDojo dojo = new TaigaDojo(100);
-            Character aoko = CreateAoko();
-
-            Console.WriteLine($"Welcome to the Taiga Dojo with {aoko.Name}!");
-            Console.WriteLine($"Initial Mana: {aoko.Resources.Mana}, Dojo HP: {dojo.TargetHP}\n");
-        }
-
-    }
+    return aoko;
 }
